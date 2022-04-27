d3b1dc319cd60802a10ac5acfb32ba00969e8e80
Erik Woodworth <erik.woodworth@asmblg.io>
2022-04-27 10:06:13 -0500

    push to deployment

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 // import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 const logo = require('./images/Assemblage Logo 3.png')
 
 function App() {
+  const [ view , setView ] = useState({});
   const menuOptions = [
     {
-      text: 'Past',
-      value: 'past'
+      title: 'Who We Are',
+      description: 'Founded in 2022, we are a small research and application development shop located in the Rogers Park neighborhood of Chicago.',
+      content: []
     },
     {
-      text: 'Present',
-      value: 'present'
+      title: 'What We Do',
+      description: 'Built on the experience, expertise, and problem-solving skills of our founder, we provide services in geospatial anaylsis and visualization, planning research, and full stack application development including: the design, creation, and deployment of custom-built interactive web apps, database development and maintenance, and bug fixes and upgrades to existing applications.',
+      content: []
     },    {
-      text: 'Future',
-      value: 'future'
+      title: 'Connect With Us',
+      description: '',
+      content: []
+
     }
   ]
   
@@ -24,22 +30,45 @@ function App() {
       </div>
       <div id='bio'>
         <p>
-        We are are small, nimble consulting firm specializing in research and full stack application development for public, nonprofit, and advocacy-oriented organizations wishing to turn data into into action.
+        We are a small, nimble consulting firm specializing in geospatial analytics, research, and full stack application development for public, nonprofit, and advocacy-oriented organizations wishing to turn data into action.
         </p>
         <p>
-        Our mission is to <strong>gather</strong> together people, data, and technology in a way that moves the needle toward greater equity, inclusivity, and transparency in the various decision-making processes that shape our neighborhoods and world.
+        Our mission is to <strong>gather</strong> together people, data, and technology in a way that moves the needle toward greater equity, inclusivity, and transparency in the various decision-making processes that impact our local and global communities.
         </p>
 
       </div>
       <div id='menu'>
-        { menuOptions.map(({text, value}) => 
-          <div className='menu-option' value={value}>
-            {text}
+        { menuOptions.map(({title, description}) => 
+          <div 
+            className='menu-option' 
+            onClick={() => setView({
+              title: title,
+              description: description
+            })}
+          >
+            {title}
           </div>
         )
 
         }
       </div>
+      <div id='view-box' className={ view.title ? 'view-box-open' : 'view-box-close'}>
+        <div 
+        >
+          <span
+            className='view-box-close-button'
+            onClick={() => setView({})}
+          >X</span>
+        </div>
+        <div
+          className='view-box-header'
+        >{view.title ? view.title.toUpperCase() : ''}</div>
+        <div className='view-box-description'>
+          {view.description}
+        </div>
+      </div>
+
+
     </div>
   );
 }
